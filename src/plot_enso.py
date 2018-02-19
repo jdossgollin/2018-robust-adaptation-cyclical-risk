ddec78aa8f12021783a1a44e6dc5a03159474a30
James Doss-Gollin <james.dossgollin@gmail.com>
2018-02-19 06:49:47 -0500

    More clear ENSO plot

--- src/plot_enso.py
+++ src/plot_enso.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 enso = pd.read_csv('~/Documents/GitHub/floodsampling/floodsampling/data/ramesh2017.csv', index_col='year')
 
-enso.loc[10000:12500].plot(figsize=(10, 5), linewidth=0.5)
+# Plot Time Series
+enso.loc[10000:12500].plot(
+    figsize=(9, 5),
+    linewidth=0.5,
+    c='blue',
+    legend=False,
+    grid=True
+)
+plt.ylabel('NINO3')
+
 plt.savefig('figs/enso.pdf', bbox_inches='tight')
