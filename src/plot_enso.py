eca073b65fa7f4c2ad5d82a05d27bae910f418ff
James Doss-Gollin <james.dossgollin@gmail.com>
2018-02-23 12:52:13 -0500

    plot the ENSO time series

--- src/plot_enso.py
+++ src/plot_enso.py
@@ -1,20 +1,35 @@
+"""Open the NINO3 time series and plot a sub-set of it
+"""
+
+import argparse
+import os
+
 import numpy as np
-import pandas as pd
+import xarray as xr
 import matplotlib.pyplot as plt
 
 from floodsampling.util import get_data_path
 
-data_fname = os.path.join(get_data_path(), 'ramesh2017.csv')
-enso = pd.read_csv(data_fname, index_col='year')
+parser = argparse.ArgumentParser() #pylint: disable=C0103
+parser.add_argument("--outfile", help="the filename of the data to save")
 
-# Plot Time Series
-enso.loc[10000:12500].plot(
-    figsize=(9, 5),
-    c='blue',
-    linewidth=0.5,
-    legend=False,
-    grid=True
-)
-plt.ylabel('NINO3')
+def main():
 
-plt.savefig('figs/enso.pdf', bbox_inches='tight')
+    args = parser.parse_args()
+    data_fname = os.path.join(get_data_path(), 'ramesh2017.nc')
+    enso = xr.open_dataarray(data_fname)
+
+    # Plot Time Series
+    enso.sel(year=slice(1000, 2000)).plot(
+        figsize=(10, 4.5),
+        c='blue',
+        linewidth=0.5
+    )
+    plt.grid(True)
+    plt.xlabel('Year')
+    plt.ylabel('Annualized NINO3 Index')
+
+    plt.savefig(args.outfile, bbox_inches='tight')
+
+if __name__ == '__main__':
+    main()
