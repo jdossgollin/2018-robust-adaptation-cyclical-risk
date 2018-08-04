deacd381d43af499e239a77616fffc96866dcdac
James Doss-Gollin <james.dossgollin@gmail.com>
2018-08-04 12:51:42 -0400

    work on habanero sync

---
+++ src/habanero-rsync.sh
@@ -0,0 +1,4 @@
+# Pull data to local computer from habanero
+rsync -avz -e ssh jwd2136@habanero.rcs.columbia.edu:/rigel/cwc/users/jwd2136/STXCluster/src/codebase/data/lfv-only-bias-variance.nc  ./codebase/data/lfv-only-bias-variance.nc
+rsync -avz -e ssh jwd2136@habanero.rcs.columbia.edu:/rigel/cwc/users/jwd2136/STXCluster/src/codebase/data/lfv-only-bias-variance.nc  ./codebase/data/secular-only-bias-variance.nc
+rsync -avz -e ssh jwd2136@habanero.rcs.columbia.edu:/rigel/cwc/users/jwd2136/STXCluster/src/codebase/data/lfv-secular-bias-variance.nc  ./codebase/data/lfv-secular-bias-variance.nc
