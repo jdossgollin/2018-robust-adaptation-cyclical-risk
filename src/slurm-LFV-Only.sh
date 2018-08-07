c148b9560cb600ba3c3540a66cf4b741032ea97f
James Doss-Gollin <james.dossgollin@gmail.com>
2018-08-07 05:58:16 -0400

    Adjust slurm

---
+++ src/slurm-LFV-Only.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Run the code on a slurm server
+#
+#SBATCH --account=cwc           # The account name for the job.
+#SBATCH --job-name=LFV-ONLY     # The job name.
+#SBATCH -N 8                    # The number of nodes to use
+#SBATCH --exclusive
+#SBATCH --time=12:00:00          # The time the job will take to run.
+
+module load anaconda            # load the anaconda module
+source activate STXCluster      # activate the conda environment
+
+# run the python
+python 01-Run-LFV-Only.py
+
+# End of script
--- src/slurm-Secular-Only.sh
+++ src/slurm-Secular-Only.sh
@@ -3,17 +3,15 @@
 # Run the code on a slurm server
 #
 #SBATCH --account=cwc           # The account name for the job.
-#SBATCH --job-name=STATIONARY   # The job name.
+#SBATCH --job-name=LFV-ONLY     # The job name.
 #SBATCH -N 8                    # The number of nodes to use
 #SBATCH --exclusive
 #SBATCH --time=12:00:00          # The time the job will take to run.
 
-module load anaconda/3-4.4.0    # load the anaconda module
+module load anaconda            # load the anaconda module
 source activate STXCluster      # activate the conda environment
 
 # run the python
-python 01-Run-LFV-Only.py
 python 02-Run-Secular-Only.py
-python 03-Run-LFV-Secular.py
 
 # End of script
